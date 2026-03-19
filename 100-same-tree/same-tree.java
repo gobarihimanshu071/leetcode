d1ee1a43583d6a5394aafdc9ea3c3dc30c231888
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-03-19 20:46:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.41%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,7 +19,7 @@ class Solution {
     }
     public boolean check(TreeNode p,TreeNode q){
         if(p==null && q==null)return true;
-        if((p==null && q!=null) || (p!=null && q==null))return false;
+        if(p==null || q==null)return false;
         if(p.val!=q.val)return false;
         return check(p.left,q.left) && check(p.right,q.right);
         
