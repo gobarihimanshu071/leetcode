07ef26de8ebd5ad75f06654c12b8961757b88199
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-06-22 23:59:56 +0530

    Time: 11 ms (51.34%) | Memory: 45.3 MB (34.71%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,39 +1,12 @@
 class Solution {
 
-    public static int f(
-        int idx,
-        int nums[],
-        int target,
-        int dp[][]){
-
-        if(idx < 0){
-            return target == 0 ? 1 : 0;
-        }
-
-        if(dp[idx][target] != -1)
-            return dp[idx][target];
-
-        int notTake =
-            f(idx-1, nums, target, dp);
-
-        int take = 0;
-
-        if(nums[idx] <= target)
-            take =
-                f(idx-1,
-                  nums,
-                  target-nums[idx],
-                  dp);
-
-        return dp[idx][target]
-               = take + notTake;
-    }
-
+   
     public int findTargetSumWays(
             int[] nums,
             int target) {
 
         int sum = Arrays.stream(nums).sum();
+        int n=nums.length;
 
         if((sum + target) < 0 ||
            (sum + target) % 2 != 0)
@@ -42,14 +15,21 @@ class Solution {
         int p = (sum + target) / 2;
 
         int[][] dp =
-            new int[nums.length][p + 1];
+            new int[nums.length+1][p + 1];
 
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        dp[0][0]=1;
 
-        return f(nums.length - 1,
-                 nums,
-                 p,
-                 dp);
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<p+1;j++){
+                int nottake = dp[i-1][j];
+                int take =0;
+                if(nums[i-1]<=j){
+                    take = dp[i-1][j-nums[i-1]];
+                }
+                dp[i][j]=take +nottake;
+            }
+            
+        }
+        return dp[n][p];
     }
 }
