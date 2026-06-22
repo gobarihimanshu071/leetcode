6cc5b17cdb8a4e1fa635c1fd0c8bf309db488a6e
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-06-22 23:50:04 +0530

    Time: 6 ms (72.05%) | Memory: 45.3 MB (34.71%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,19 +1,55 @@
 class Solution {
 
-    public static void f(int idx, int nums[], int target, int sum, int count[]){
-        if(idx<0){
-            if(sum==target)count[0]++;
-            return;
+    public static int f(
+        int idx,
+        int nums[],
+        int target,
+        int dp[][]){
+
+        if(idx < 0){
+            return target == 0 ? 1 : 0;
         }
-        f(idx-1,nums,target,sum+nums[idx],count);
-        f(idx-1,nums,target,sum-nums[idx],count);
-        return; 
+
+        if(dp[idx][target] != -1)
+            return dp[idx][target];
+
+        int notTake =
+            f(idx-1, nums, target, dp);
+
+        int take = 0;
+
+        if(nums[idx] <= target)
+            take =
+                f(idx-1,
+                  nums,
+                  target-nums[idx],
+                  dp);
+
+        return dp[idx][target]
+               = take + notTake;
     }
 
-    public int findTargetSumWays(int[] nums, int target) {
-        int sum=0;
-        int[] count=new int[1];
-        f(nums.length-1, nums,target,sum,count);
-        return count[0];
+    public int findTargetSumWays(
+            int[] nums,
+            int target) {
+
+        int sum = Arrays.stream(nums).sum();
+
+        if((sum + target) < 0 ||
+           (sum + target) % 2 != 0)
+            return 0;
+
+        int p = (sum + target) / 2;
+
+        int[][] dp =
+            new int[nums.length][p + 1];
+
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return f(nums.length - 1,
+                 nums,
+                 p,
+                 dp);
     }
 }
