09e227ad674bdbbe1365bff5c233e65a753faa99
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-06-24 03:17:19 +0530

    Time: 15 ms (81.87%) | Memory: 46.8 MB (32.62%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -4,24 +4,26 @@ class Solution {
 
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp = new int[n+1][amount+1];
+        int[] dp = new int[amount+1];
         int INF = 1000000000;
         for(int i=0;i<amount+1;i++){
-            dp[n][i]=INF;
+            dp[i]=INF;
         }
-        dp[n][0]=0;
+        dp[0]=0;
         for(int i=n-1;i>=0;i--){
+            int[] curr = new int[amount+1];
             for(int a=0;a<=amount;a++){
-                int nottake = dp[i+1][a];
+                int nottake = dp[a];
                 int take = INF;
                 if(coins[i]<=a){
-                    take = 1+dp[i][a-coins[i]];
+                    take = 1+curr[a-coins[i]];
                 }
-                dp[i][a]=Math.min(take,nottake);
+                curr[a]=Math.min(take,nottake);
             }
+            dp=curr;
 
         }
-        return dp[0][amount]>=INF ? -1 : dp[0][amount];
+        return dp[amount]>=INF ? -1 : dp[amount];
         
     }
 }
