8ec2293e6d305287d8c1e485dfc2533dc0a43b92
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-06-24 04:15:01 +0530

    Time: 5 ms (99.90%) | Memory: 42.8 MB (94.86%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -7,18 +7,16 @@ class Solution {
         int[] dp = new int[amount+1];
         dp[0]=1;
 
-        for(int i=n-1;i>=0;i--){
-            int[] curr=new int[amount+1];
-            for(int a=0;a<=amount;a++){
-                int nottake = dp[a];
-                int take =0;
-                if(coins[i]<=a){
-                    take = curr[a-coins[i]];
-                }
-                curr[a]=take+nottake;
-            }
-            dp=curr;
-        }
+        for(int coin : coins){
+
+    for(int a = coin;
+            a <= amount;
+            a++){
+
+        dp[a] +=
+            dp[a - coin];
+    }
+}
         return dp[amount];
     }
 }
