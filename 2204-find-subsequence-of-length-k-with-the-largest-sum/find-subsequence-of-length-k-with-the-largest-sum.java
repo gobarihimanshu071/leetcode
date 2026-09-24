489a03c0db06015a08daf4388382812cd057de40
gobarihimanshu071 <152051380+gobarihimanshu071@users.noreply.github.com>
2026-09-24 14:05:01 +0530

    Time: 9 ms (52.58%) | Memory: 46.7 MB (15.96%) - LeetSync

---
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        for(int i=0;i<nums.length;i++){
+            pq.offer(new int[]{nums[i],i});
+            if(pq.size()>k)pq.remove();
+        }
+        int selected[][] = new int[k][2];
+        for(int i=0;i<k;i++){
+            selected[i]=pq.poll();
+        }
+        Arrays.sort(selected,(a,b)->a[1]-b[1]);
+        int ans[] =new int[k];
+        for(int i=0;i<k;i++){
+            ans[i]=selected[i][0];
+        }
+        return ans;
+    }
+}
